306a15906fc04ef7002d6cda140a760a82197093
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-08-16 21:25:24 +0000

    Feat: Added litjs and fix eslint and tsconfig issues

---
+++ assets/components/SimpleGreeting.ts
@@ -0,0 +1,24 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("simple-greeting")
+export class SimpleGreeting extends LitElement {
+   static styles = css`
+      :host {
+         color: blue;
+      }
+   `;
+
+   @property()
+   firstname?: string = "World";
+
+   render() {
+      return html`<p>Hello, ${this.firstname}!</p>`;
+   }
+}
+
+declare global {
+   interface HTMLElementTagNameMap {
+      "simple-greeting": SimpleGreeting;
+   }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ Encore
     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
     */
    .addEntry("app", "./assets/app.ts")
+   .addEntries({
+      SimpleGreeting: "./assets/components/SimpleGreeting.ts",
+   })
 
    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
    .splitEntryChunks()
