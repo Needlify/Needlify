83e9f19be8ed6fece5cbecdef2532c36a0846778
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-08-19 23:36:06 +0000

    Fix: Updated tsconfig to remove errors

--- assets/components/SimpleGreeting.ts
+++ assets/components/SimpleGreeting.ts
@@ -1,19 +1,22 @@
-import { css, html, LitElement } from "lit";
+import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("simple-greeting")
 export class SimpleGreeting extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
     static styles = css`
         :host {
             color: blue;
         }
     `;
 
-    @property({ type: String })
-    firstname?: string = "World";
+    // Declare reactive properties
+    @property()
+    name?: string = "World";
 
+    // Render the UI as a function of component state
     render() {
-        return html`<p>Hello, ${this.firstname}!</p>`;
+        return html`<p>Hello, ${this.name}!</p>`;
     }
 }
 
