5bbdc8a0f7c0c3e1133953ca4408690eb47b33c2
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-08-22 20:16:06 +0000

    Fix: Updated twig file formater

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class FeedController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('pages/feed.html.twig');
     }
 }
