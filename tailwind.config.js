8be5b4d65268c25a8ec25a73781d4a5090915095
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-09-11 21:38:18 +0000

    Feat: Added assets (base style, fonts, header) + tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./assets/**/*.ts", "./templates/**/*.html.twig"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 };
