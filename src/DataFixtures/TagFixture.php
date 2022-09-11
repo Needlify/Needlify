53e6f0c61b24c463560a2899c25b94e95c91ff22
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-09-11 12:37:42 +0000

    Fix: Updated tag and topic fixtures

--- src/DataFixtures/TagFixture.php
+++ src/DataFixtures/TagFixture.php
@@ -5,22 +5,15 @@ namespace App\DataFixtures;
 use App\Entity\Tag;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 
 class TagFixture extends Fixture
 {
-    private $faker;
-
-    public function __construct()
-    {
-        $this->faker = Factory::create();
-    }
-
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 5; ++$i) {
+        $tags = ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'];
+        foreach ($tags as $tagName) {
             $tag = new Tag();
-            $tag->setName($this->faker->word());
+            $tag->setName($tagName);
 
             $manager->persist($tag);
         }
--- src/DataFixtures/TopicFixture.php
+++ src/DataFixtures/TopicFixture.php
@@ -5,24 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\Topic;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 
 class TopicFixture extends Fixture
 {
-    private $faker;
-
-    public function __construct()
-    {
-        $this->faker = Factory::create();
-    }
-
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 5; ++$i) {
-            $tag = new Topic();
-            $tag->setName($this->faker->word());
+        $topics = ['topic1', 'topic2', 'topic3', 'topic4', 'topic5'];
+        foreach ($topics as $topicName) {
+            $topic = new Topic();
+            $topic->setName($topicName);
 
-            $manager->persist($tag);
+            $manager->persist($topic);
         }
         $manager->flush();
     }
