787b4fde2a3be3d21ed06233ef9a78ffa9ba74ae
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-09-20 18:49:58 +0000

    Feat: Added posts argument to twig view

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('instanceOf', [$this, 'isInstanceof']),
+        ];
+    }
+
+    public function isInstanceof($element, $meta): bool
+    {
+        return $element instanceof $meta;
+    }
+}
