04dc25c34f92fd26d308ced4b2b37b83e91d8e63
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-10-07 23:21:36 +0000

    Feat: Added classifier types (php and ts) #6

--- assets/types.d.ts
+++ assets/types.d.ts
@@ -1,5 +1,7 @@
 export type ThreadTypeVariation = "article" | "moodline" | "event";
+export type ClassifierTypeVariation = "topic" | "tag";
 export type ThreadIconAssociationType = Record<ThreadTypeVariation, { icon: string; color: string }>;
+export type ClassifierTypeVariationType = Record<string, ClassifierTypeVariation>;
 
 export type Classifier = {
     name: string;
---
+++ src/Service/ClassifierType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Service;
+
+enum ClassifierType: string
+{
+    case TOPIC = 'topic';
+    case TAG = 'tag';
+}
