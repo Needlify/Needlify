8180a93817e60706248150e475ba4ff466b12051
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-10-09 16:27:18 +0000

    Fix: Updated base ts file path

--- webpack.config.ts
+++ webpack.config.ts
@@ -31,7 +31,7 @@ Encore.setOutputPath("public/build/")
         "page:feed": "./assets/styles/pages/feed.scss",
 
         /* Typescript files */
-        "file:base": "./assets/base.ts",
+        "file:base": "./assets/files/base.ts",
 
         /* Custom Elements */
         "component:Timeline": "./assets/vue/Timeline",
