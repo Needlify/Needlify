02a54b9c19ec7c953a0c395d2b2a8c43a20339cd
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-10-19 19:15:44 +0000

    Fix: #29 Fix cs check  in RegisterController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,9 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
                 (new TemplatedEmail())
                     ->from(new Address('noreply@needlify.com', 'Needlify - Noreply'))
                     ->to($user->getEmail())
