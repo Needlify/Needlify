223a26d4e984477a9768f6a8e0600e2f4d855adc
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-11-04 20:09:27 +0000

    Feat: #37 Finalisation page article avec responsive

--- assets/files/swup.ts
+++ assets/files/swup.ts
@@ -1,20 +1,20 @@
-import Swup from "swup";
-import SwupProgressPlugin from "@swup/progress-plugin";
-import SwupHeadPlugin from "@swup/head-plugin";
-import "../styles/swup.scss";
+// import Swup from "swup";
+// import SwupProgressPlugin from "@swup/progress-plugin";
+// import SwupHeadPlugin from "@swup/head-plugin";
+// import "../styles/swup.scss";
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-const swup = new Swup({
-    plugins: [
-        new SwupProgressPlugin({
-            className: "swup-progress-bar",
-            transition: 300,
-            delay: 300,
-            initialValue: 0.25,
-            hideImmediately: true,
-        }),
-        new SwupHeadPlugin(),
-    ],
-    containers: ["#main-container"],
-    // cache: false, // TODO Checker la pertinence de cette option
-});
+// // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+// const swup = new Swup({
+//     plugins: [
+//         new SwupProgressPlugin({
+//             className: "swup-progress-bar",
+//             transition: 300,
+//             delay: 300,
+//             initialValue: 0.25,
+//             hideImmediately: true,
+//         }),
+//         new SwupHeadPlugin(),
+//     ],
+//     containers: ["#main-container"],
+//     // cache: false, // TODO Checker la pertinence de cette option
+// });
