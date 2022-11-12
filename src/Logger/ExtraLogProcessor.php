e76efb5d19baf366f2ae15a4a0220c6bf926f722
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-11-12 18:46:10 +0000

    Fix: #23 Removed unused code

--- src/Logger/ExtraLogProcessor.php
+++ src/Logger/ExtraLogProcessor.php
@@ -25,17 +25,6 @@ class ExtraLogProcessor
         $this->requestStack = $requestStack;
     }
 
-    private function mapped_implode($glue, $array, $symbol = '=')
-    {
-        return implode($glue, array_map(
-            function ($k, $v) use ($symbol) {
-                return $k . $symbol . $v;
-            },
-            array_keys($array),
-            array_values($array)
-        ));
-    }
-
     public function __invoke(LogRecord $record)
     {
         $serverParams = $this->requestStack->getCurrentRequest()->server ?? null;
