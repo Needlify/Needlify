043e791f40d44568ca103774b53fdc6bafb85751
MrAnyx <1ptitigre@gmail.com>
2022-11-20 16:22:36 +0000

    Fix: #36 Updated AppExtension test

--- tests/Twig/AppExtensionTest.php
+++ tests/Twig/AppExtensionTest.php
@@ -9,18 +9,8 @@
 
 namespace App\Tests\Twig;
 
-use App\Entity\Topic;
-use App\Twig\AppExtension;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class AppExtensionTest extends KernelTestCase
 {
-    public function testIsInstanceOf()
-    {
-        $appExtension = new AppExtension();
-        $element = new Topic();
-        $element->setName('Test_topic');
-        $this->assertTrue($appExtension->isInstanceof($element, 'App\\Entity\\Topic'), true);
-        $this->assertTrue($appExtension->isInstanceof($element, Topic::class), true);
-    }
 }
